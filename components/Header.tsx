919515d47a943c58cbbac01647ce21cf1e216282
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-16 13:59:55 -0400

    Removed extra padding and stick element on top layer.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="sticky top-0 bg-teal-50 shadow-sm shadow-teal-950/10 border-b border-teal-950/10 p-6 ">
+    <header className="bg-teal-50 shadow-sm shadow-teal-950/10 border-b border-teal-950/10">
       <div className="wrapper p-6">
         <p>This is a Header</p>
       </div>
