d0680aef0be1641356a64e73df6c2d3cddc5a605
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-16 13:58:45 -0400

    Added a global container ensure consistent  alignment with text.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-teal-950">
-      <div className="container mx-auto max-w-[80%] p-6">
+      <div className="wrapper p-6">
         <p className="text-teal-50">This is a Footer</p>
       </div>
     </footer>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="sticky top-0 bg-teal-50 shadow-sm shadow-teal-950/10 border-b border-teal-950/10 p-6 ">
-      <div className="container mx-auto max-w-[80%]">
+      <div className="wrapper p-6">
         <p>This is a Header</p>
       </div>
     </header>
