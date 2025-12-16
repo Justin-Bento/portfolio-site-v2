09184178b811e83cabd068a3525a4a30a1845974
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-16 13:03:56 -0400

    Replaced boilerplate code with new jsx code and some tailwind styling.

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="container  max-w-[90%] mx-auto p-6 min-h-dvh grid place-items-center">
+      <section className="*:lg:flex *:items-centre *:lg:justify-center *:text-center">
+        <span className="block  mb-12">
+          <Image src="" alt="" className="size-24 bg-gray-400 rounded-full" />
+        </span>
+        <h1 className="">Hi, My name is Justin Bento.</h1>
+        <p className="">
+          I recently graduated from Humber College’s Multimedia Design and
+          Development program.
+        </p>
+        <p className="mt-6 max-w-prose mx-auto">
+          My goal is to specializing in website development and multimedia
+          design. During my time at Humber, I learned a lot about design and
+          development and continued my education in website development through
+          Harvard CS50 and The Odin Project.
+        </p>
+        <span className="mt-12 block lg:gap-x-4">
+          <button className="">View Projects</button>
+          <button className="">Let's Talk</button>
+        </span>
+      </section>
+    </main>
   );
 }
