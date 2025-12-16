b93ab3b8623eea92b6530b42138bd757e0df7ac1
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-16 13:45:03 -0400

    Adjusted style of the website to fit teal colour scheme.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-teal-950">
-      <p className="">Footer</p>
+      <div className="container mx-auto max-w-[80%] p-6">
+        <p className="text-teal-50">This is a Footer</p>
+      </div>
     </footer>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
-    <header className="bg-teal-50">
-      <p>This is a Header</p>
+    <header className="sticky top-0 bg-teal-50 shadow-sm shadow-teal-950/10 border-b border-teal-950/10 p-6 ">
+      <div className="container mx-auto max-w-[80%]">
+        <p>This is a Header</p>
+      </div>
     </header>
   );
 }
