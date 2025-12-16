714a4ae13947db6dd6b2819942d8a6e0392161b8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-16 13:26:23 -0400

    Styled typography to achieve center alignment while maintaining appropriate hierarchy.

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="container  max-w-[90%] mx-auto p-6 min-h-dvh grid place-items-center">
-      <section className="*:lg:flex *:items-centre *:lg:justify-center *:text-center">
+      <section className="*:lg:flex *:items-centre *:lg:justify-center *:lg:text-center">
         <span className="block  mb-12">
           <Image src="" alt="" className="size-24 bg-gray-400 rounded-full" />
         </span>
-        <h1 className="">Hi, My name is Justin Bento.</h1>
-        <p className="">
-          I recently graduated from Humber College’s Multimedia Design and
+        <h1 className="text-lg font-medium text-teal-700 capitalize mb-4">
+          Hi, My name is Justin Bento.
+        </h1>
+        <p className="scroll-m-20 text-4xl leading-12 font-extrabold tracking-tight text-balance max-w-prose capitalize">
+          I'm A recent graduated from Humber College’s Multimedia Design &
           Development program.
         </p>
-        <p className="mt-6 max-w-prose mx-auto">
-          My goal is to specializing in website development and multimedia
-          design. During my time at Humber, I learned a lot about design and
-          development and continued my education in website development through
-          Harvard CS50 and The Odin Project.
+        <p className="mt-6 max-w-[80ch] mx-auto text-lg leading-9 text-balance">
+          My goal is to become a professional website developer that knows how
+          to design. During my time at Humber, I learned a lot about design and
+          development after I graudate I plan on continuing my education in
+          website development through Harvard CS50 and The Odin Project and land
+          a job.
         </p>
         <span className="mt-12 block lg:gap-x-4">
           <button className="">View Projects</button>
